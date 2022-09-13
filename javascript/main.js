8972eb0011c737cd0ba6939ba4bef8a7c33cd613
Adi Popa X <109682966+adipopax@users.noreply.github.com>
2022-09-13 21:15:56 +0300

    .

--- javascript/main.js
+++ javascript/main.js
@@ -82,7 +82,7 @@ sum++;
 // sum = sum + 10;
 // sum = sum / 10 * 2;
 // sum = sum / (10 * 2);
-console.log(sum);
+// console.log(sum);
 
 const hello = 'Hello'
 const world = 'World'
@@ -93,23 +93,23 @@ const world = 'World'
 let name = 'Tom';
 name += ' Jerry';
 
-console.log(name);
+// console.log(name);
 
 // Template literal -> backticks -> it's let us interpolate expression via '${}'
-console.log(
-`Tom 
+// console.log(
+// `Tom 
 
-            Jerry
-`);
+//             Jerry
+// `);
 // Can put anything
 const stringNumber = '123';
-console.log(`${hello} ${world} ${1 + 2 + 3 + 4 + 5}`);
-console.log(`${hello} ${world} ${Number(stringNumber)}`);
+//console.log(`${hello} ${world} ${1 + 2 + 3 + 4 + 5}`);
+//console.log(`${hello} ${world} ${Number(stringNumber)}`);
 
-console.log(Number('123') + 3);
+//console.log(Number('123') + 3);
 // bug! |
-console.log('123' + 3);
-console.log(123 + 3);
+//console.log('123' + 3);
+//console.log(123 + 3);
 
 /* 
     Conditional statements - crossroad
@@ -120,9 +120,9 @@ console.log(123 + 3);
 const testNumber = 13;
 
 if(testNumber % 2 == 0){
-    console.log('Your number is even');
+    //console.log('Your number is even');
 } else {
-    console.log('Your number is odd');
+    //console.log('Your number is odd');
 }
 
 /*
@@ -135,47 +135,219 @@ if(testNumber2 % 2 == 0){
 }
 */
 
-const testNumber3 = prompt('Please type in your test number');
+const testNumber3 = 33; //prompt('Please type in your test number');
 
 if(testNumber3 % 2 == 0){
-    console.log('Your number is even');
+    //console.log('Your number is even');
 } else {
-    console.log('Your number is odd');
+    //console.log('Your number is odd');
 }
 
-console.log(testNumber3 + 100);
+//console.log(testNumber3 + 100);
 
 
 // +prompt('Please type in your test number') same thing as Number() (Number is more explicit)
 
-const userInput = Number(prompt('Please type in your test number (User Input)'));
+const userInput =  33 ;//Number(prompt('Please type in your test number (User Input)'));
 
 if(userInput % 2 == 0){
-    console.log('Your number is even');
+    //console.log('Your number is even');
 } else {
-    console.log('Your number is odd');
+    //console.log('Your number is odd');
 }
 
-console.log(userInput + 100);
+//console.log(userInput + 100);
 
+
+// Conditional statements - cross road
+
+// How would we determinate is a number is odd pr even
 
 // MORE EXAMPLES
 
-const userInput2 = prompt('Please type in your test number (userInputAsNumber)');
+const userInput2 =   33;//prompt('Please type in your test number (userInputAsNumber)');
 const userInputAsNumber = Number(userInput2);
 // Converts expression to boolean
 // Truthy or Falsy values
 const isEven = userInput2 % 2 == 0;
 
 // if is odd log odd if is even log even
-if(userInput2 % 2 == 0){
-    console.log('Your number is even');
-} else {
-    console.log('Your number is odd');
+    if(userInput2 % 2 == 0){
+   // console.log('Your number is even');
+} 
+    else {
+   // console.log('Your number is odd');
 }
 
-console.log(userInputAsNumber + 100);
+//console.log(userInputAsNumber + 100);
+//console.log(userInput + 200);
 
 // never use == always use ===
 // == it verify if is true
 // == it verify if it true and if is the type of data is correct
+
+/*
+    if user inputs a number lower than 10 show hello
+    if user inputs a number lower than 20 show hi
+    if user inputs a number lower than 30 show hello world
+    if the number is anything bigger than 30 show Awesome
+    if the number is equal with 30 show something
+*/
+
+let userInput3 =  33;//prompt('Please input a number : ');
+
+// debugger;
+
+if (userInput3 === '' ) {
+    userInput3 = promptInvalidUserInput();
+} 
+
+const userInput3AsNumber = Number(userInput3);
+
+if (isNaN (userInput3AsNumber)) {
+    userInput3AsNumber = Number(promptInvalidUserInput());
+}
+
+if (userInput3 < 10 ) {
+    console.log('hello');
+} else if (userInput3 < 20) {
+    console.log('hi');
+} else if (userInput3 < 30) {
+    console.log('hello world');
+} else if (userInput3 >= 30) {
+   console.log('Awesome');
+} else {
+    console.error('Your input was not a number')
+}
+
+/**
+ * By default all functions produce undefined as result;
+ * In order for a function to result as we need to: return;
+ * Pure functions -> doesn't modify things from outside only relies on inputs to produce a value;
+ *  */
+
+function promptInvalidUserInput() {
+    console.log(' You entered invalid information ! Please enter a number');
+    const userInput = prompt(' You entered invalid information ! Please enter a number');
+
+    return userInput; 
+}
+
+/**
+ * If user input is between 10 and 15 display Hello users
+ * 
+ * If user input is 20 or 30 display YES!
+ */
+
+/**
+ * Logical operators
+ * and &&
+ * or ||
+ */
+
+const userInput4 = 15;
+
+if (userInput4 >= 10 && userInput4 <= 15) {
+    console.log('Hello user');
+}
+
+if (userInput4 === 20 || userInput4 === 30) {
+    console.log('YES!');
+}
+
+/**
+ * When checking for equality between anything 
+ * If user input is 20 show Yes 20
+ * If user input is 30 show Yes 30
+ * If user input is 40 show Yes 40
+ * else 50
+ * 
+ * is user input is 50 or 60 Yes something
+ * 
+ * in this case if can be case
+ * and             else can be default
+ */
+
+const userInput5 = 60;
+
+switch (userInput5) {
+    case 20: {
+        console.log('Yes 20');
+        break;
+        // break is important if there's no break it will execute the code as is should
+    }
+    case 30: {
+        console.log('Yes 30');
+        break;
+    }
+    case 40: {
+        console.log('Yes 40');
+        break;
+    }
+    case 50:
+    case 60: {
+        console.log('Yes something');
+        break;
+    }
+    default: {
+        console.log('Yes 50');
+    }
+}
+
+
+/**
+ * Complex data types :
+ * 
+ * Array's - Will contain primitive values or other complex data types
+ * Object's - Will contain primitive values or other complex data types
+ */
+
+const shoppingList = ['Turkey', 'Bread', 'Meat', 'Vegetables', 'Fruits'];
+// All items are stored on an index 
+/**
+ * Bread - 0
+ * Meat - 1
+ * Vegetables - 2
+ * Fruits - 3
+ */
+
+ console.log(shoppingList);
+
+// Adds items to the end of the list
+shoppingList.push('Something new')
+
+// Adds items to the beginning of the list
+shoppingList.unshift('Start of the list');
+
+// Remove element from the end of the list
+shoppingList.pop();
+
+// Remove first element from the the list
+shoppingList.shift();
+
+// Remove the element based on the index
+// shoppingList.splice(2, 1);
+
+// find the index of an item
+const toRemoveIndex = shoppingList.indexOf('Vegetables');
+shoppingList.splice(toRemoveIndex, 1);
+
+console.log(shoppingList);
+
+/**
+ * const shoppingList = ['Turkey', 'Bread', 'Meat', 'Vegetables', 'Fruits'];
+ * 
+ * ['Turkey: 0', 'Bread: 1', 'Meat: 2', 'Vegetables: 3', 'Fruits: 4'];
+ */
+
+const quantity = [3, 10, 6, 20, 15]
+const shoppingListWithQuantity = [];
+
+for (let index = 0; index < shoppingList.length; index++) {
+    const product = shoppingList[index];
+    const qnt = quantity[index];
+
+    shoppingListWithQuantity.push(`${product}: ${qnt}`);
+}
+
+console.log(shoppingListWithQuantity);
