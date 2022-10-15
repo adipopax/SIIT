4d0fcd1bc392ca814c6cd4de14e36cf0e2a55b7e
Adi Popa X <109682966+adipopax@users.noreply.github.com>
2022-10-15 11:06:35 +0300

    .

--- todo/main.js
+++ todo/main.js
@@ -1,33 +1,34 @@
 
 /**
- * 1. We should list (read) all ToDo's that are stored on the server (GET /todos)
- * 2. Create a ToDo functionality (POST /todos)
- * 3. Delete ToDo's (DELETE /todos/:id)
- * 4. Update ToDo (PATCH/PUT /todos/:id)
+ * 1. We should list (read) all todos that are stored on the server (GET /todos)
+ * 2. Create todo functionality (POST /todos)
+ * 3. Delete todo (DELETE /todos/:id)
+ * 4. Update todo (PATCH/PUT /todos/:id)
  */
 
-const url = 'http://localhost:3000';
-const endpoint = '/todos';
-
-fetch(url + endpoint)
-    .then((response) => response.json())
-    .then((todos) => listTodos(todos))
-
-function listTodos(todos) {
-    const template =  document.querySelector('#todo-card');
-    const output = document.querySelector('[]')
-    for (const todo of todos) {
-        const clonedTemplate = template.textContent.cloneNode(true);
-        const h3 = clonedTemplate.querySelector('[data-todoTitle]');
-        h3.textContent = todos.title
-
-        const description = clonedTemplate.querySelector('[data-todoDescription]');
-        description.testContent = todo.description;
-        const deadline = clonedTemplate.querySelector('[todo-todoDeadline]');
-        deadline.textContent = todo.deadline;
-
-        output.appendChild(clonedTemplate);
+ const url = 'http://localhost:3000';
+ const endpoint = '/todos';
+ 
+ fetch(url + endpoint)
+   .then((response) => response.json())
+   .then((todos) => listTodos(todos))
+ 
+ function listTodos(todos) {
+   const template = document.querySelector('#todo-card');
+   const output = document.querySelector('[data-todoList]');
+   
+   for (const todo of todos) {
+     const clonedTemplate = template.content.cloneNode(true);
+     
+     const h3 = clonedTemplate.querySelector('[data-todoTitle]');
+     h3.textContent = todo.title;
+ 
+     const description = clonedTemplate.querySelector('[data-todoDescription]');
+     description.textContent = todo.description;
+     
+     const deadline = clonedTemplate.querySelector('[data-todoDeadline]');
+     deadline.textContent = todo.deadline;
+    
+      output.appendChild(clonedTemplate);
     }
-
-
 }
