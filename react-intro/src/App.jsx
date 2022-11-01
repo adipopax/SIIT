557e45ac8b4110cf95a5d243deb046989fb8e840
Adi Popa X <109682966+adipopax@users.noreply.github.com>
2022-11-01 17:25:12 +0200

    .

--- react-intro/src/App.jsx
+++ react-intro/src/App.jsx
@@ -1,60 +1,55 @@
-// import logo from './logo.svg';
+
+import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-// import { useCallback } from 'react';
 
-// const buttons = [
-//   { text: 'red' className: 'btn-red' },
-//   { text: 'blue' className: 'btn-blue' },
-//   { text: 'yellow' className: 'btn-yellow' },
-// ]
+const buttons = [
+  { text: 'red', className: 'btn-red' },
+  { text: 'blue', className: 'btn-blue' },
+  { text: 'yellow', className: 'btn-yellow' },
+]
 
 function App() {
   // State hook
+  // let backgroundColor = 'bg-blue';
 
   const [backgroundColor, setBackgroundColor] = useState('bg-blue');
   // const [width, setWidth] = useState(500);
   // const [todos, setTodos] = useState([]);
-
-  // let backgroundColor = 'bg-blue';
-
+  
   function buttonClickHandler(event) {
+    
     if (event.target.className.includes('btn-red')) {
-      // console.log('red');
       // backgroundColor = 'bg-red';
       setBackgroundColor('bg-red');
       // setWidth('300px');
     }
 
     if (event.target.className.includes('btn-blue')) {
-      // console.log('blue');
       // backgroundColor = 'bg-blue';
       setBackgroundColor('bg-blue');
-      // setWidth('400px');
+      // setWidth({ hello: 'world' });
+      // setTodos([{}, {}, {}]);
     }
 
     if (event.target.className.includes('btn-yellow')) {
-      // console.log('yellow');
       // backgroundColor = 'bg-yellow';
       setBackgroundColor('bg-yellow');
       // setWidth('500px');
     }
-
-  };
+  }
 
   return (
-    <>
-    {/* <main className={'backgroundColor'} style={{ width }} ></main> */}
-      <main className={'backgroundColor'} >
-        <section>
-          <p>Color Picker</p>
-          {/* Button( { text: 'red' } ) */}
-          <Button text='red' className='btn-red' onClick={buttonClickHandler} />
-          <Button text='blue' className='btn-blue' onClick={buttonClickHandler} />
-          <Button text='yellow' className='btn-yellow' onClick={buttonClickHandler} />
-        </section>
-      </main>
-    </>
+    // <main className={backgroundColor} style={{ width }}>
+    <main className={backgroundColor}>
+      <section>
+        <p>Color Picker</p>
+        {/* Button({ text: 'red' }) */}
+        <Button text="red" className="btn-red" onClick={buttonClickHandler} />
+        <Button text="blue" className="btn-blue" onClick={buttonClickHandler} />
+        <Button text="yellow" className="btn-yellow" onClick={buttonClickHandler} />
+      </section>
+    </main>
   );
 }
 
@@ -63,9 +58,9 @@ export default App;
 function Button({ text, className, onClick }) {
   // const { text, prop2 } = props;
 
-  return(
-    // <button className={'btn ' + className} >
-    <button className={`btn ${className}`} onClick={onClick} >
+  return (
+    // <button className={'btn ' + className}>
+    <button className={`btn ${className}`} onClick={onClick}>
       {text}
     </button>
   )
--- react-intro/src/index.js
+++ react-intro/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './css-reset.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './css-reset.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // this is JSX -> HTML in JavaScript
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// This is JSX -> HTML in JavaScript
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
