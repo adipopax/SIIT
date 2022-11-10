a3dd7057671180b9d4313523c249e93b0309baba
Adi Popa X <109682966+adipopax@users.noreply.github.com>
2022-11-10 21:30:11 +0200

    .

--- react-movies/src/pages/movie-list/MovieCardComponent.jsx
+++ react-movies/src/pages/movie-list/MovieCardComponent.jsx
@@ -1,20 +1,62 @@
 import { Link } from 'react-router-dom';
 import movieCardStyles from './MovieCardComponent.module.css';
 
+// Cart is a list []
+// Each item in tha cart should be an object 
+
 export function MovieCardComponent(props) {
+  const cartUrl = 'http://localhost:3001/cart';
   const { Title, Year, Type, Poster, id } = props;
+
+  function addToCart(event) {
+    event.preventDefault();
+
+    fetch(`${cartUrl}?movieId=${id}`)
+      .then(response => response.json())
+      .then(cartMovies => {
+        const [cartMovie] = cartMovies;
+
+        if(cartMovie) {
+          updateCartMovieQuantity(cartMovie);
+        } else {
+          createCartMovie();
+        }
+      });
+  }
+
+  function createCartMovie() {
+    fetch(`${cartUrl}`, {
+      method: 'POST',
+      body: JSON.stringify({ movieId: id, quantity: 1 }),
+      headers: {
+        'Content type': 'application/json'
+      }
+    });
+  }
+  
+  function updateCartMovieQuantity(cartMovie) {
+    fetch(`${cartMovie}/${cartMovie.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({ quantity: cartMovie.quantity + 1 }),
+      headers: {
+        'Content type': 'application/json'
+      }
+    });
+  }
 
   return (
     <Link to={`/movie-details/${id}`}>
       <li>
         <article className={movieCardStyles['movie-card']}>
           <h3 className={movieCardStyles['movie-card--title']}>{Title}</h3>
-
+  
           <span>{Year}</span>
           <span>{Type}</span>
-
+  
           <img src={Poster} alt="Movie poster" />
         </article>
+  
+        <button onClick={addToCart}>Add to cart</button>
       </li>
     </Link>
   );
